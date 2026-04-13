054481f53d463bf680fd9e7103a2a196f89358ba
rufuapal1600 <rufuapal1600@users.noreply.github.com>
2026-04-13 03:09:25 +0000

    add accountant category to find my team

--- api/team.js
+++ api/team.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
     lender: { deny: ['real_estate_agency'] },
     dscr: { deny: ['real_estate_agency'] },
     title: { deny: ['real_estate_agency'] },
-    contractor: { deny: ['real_estate_agency'] }
+    contractor: { deny: ['real_estate_agency'] },
+    accountant: { deny: ['real_estate_agency'] }
   };
 
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
